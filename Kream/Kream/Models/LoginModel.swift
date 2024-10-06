acd96ad45d0333ef8eca34420b3eba1e4bf16de3
kayeon2 <tjsrkdus@gmail.com>
2024-10-06 18:48:35 +0900

    폴더 및 파일 생성

---
+++ Kream/Kream/Models/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  Kream
+//
+//  Created by 선가연 on 10/6/24.
+//
+
+import Foundation
---
+++ Kream/Kream/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Kream
+//
+//  Created by 선가연 on 10/6/24.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

b54336028b6cbc6f63be4dd2ca79a4306bd1bffb
kayeon2 <tjsrkdus@gmail.com>
2024-10-06 16:12:14 +0900

    프로젝트 파일 생성

---
+++ Kream/Kream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kream
+//
+//  Created by 선가연 on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

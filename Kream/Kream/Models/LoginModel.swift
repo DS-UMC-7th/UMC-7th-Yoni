d7e393b8f81dd9e8a31b79cc46abbed2f9bbd4ff
kayeon2 <tjsrkdus@gmail.com>
2024-10-31 18:18:28 +0900

    refactor: LoginView Model 수정

--- Kream/Kream/Models/LoginModel.swift
+++ Kream/Kream/Models/LoginModel.swift
@@ -5,7 +5,26 @@
 //  Created by 선가연 on 10/6/24.
 //
 
-struct LoginModel {
-    var id: String
-    var pwd: String
+import Foundation
+
+class LoginModel {
+    private let userDefaults = UserDefaults.standard
+    private let userIdKey: String = "userId"
+    private let userPasswordKey: String = "userPassword"
+    
+    // 유저 아이디와 비밀번호 저장
+    public func saveUserCredentials(id: String, password: String) {
+        userDefaults.set(id, forKey: userIdKey)
+        userDefaults.set(password, forKey: userPasswordKey)
+    }
+    
+    // 유저 아이디 로드
+    public func loadUserId() -> String? {
+        return userDefaults.string(forKey: userIdKey)
+    }
+    
+    // 유저 비밀번호 로드
+    public func loadUserPassword() -> String? {
+        return userDefaults.string(forKey: userPasswordKey)
+    }
 }

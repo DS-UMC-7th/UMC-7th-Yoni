2dc48a21770a197e9f89e19a800579c1ac3c9c64
kayeon2 <tjsrkdus@gmail.com>
2024-11-04 18:11:05 +0900

    feat: 추천 세그먼트 외 빈 화면 표시

--- Kream/Kream/ViewControllers/HomeViewController.swift
+++ Kream/Kream/ViewControllers/HomeViewController.swift
@@ -8,19 +8,39 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     private let homeView = HomeView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = homeView
+        setupAction()
         setupDelegate()
     }
-
+    
+    private func setupAction() {
+        homeView.segmentedControl.addTarget(
+            self,
+            action: #selector(segmentedControlValueChanged(segment:)),
+            for: .valueChanged
+        )
+    }
+    
     private func setupDelegate() {
         homeView.menuCollectionView.dataSource = self
     }
     
+    @objc
+    private func segmentedControlValueChanged(segment: UISegmentedControl) {
+        if segment.selectedSegmentIndex == 0 {
+            homeView.bannerImage.isHidden = false
+            homeView.menuCollectionView.isHidden = false
+        }
+        else {
+            homeView.bannerImage.isHidden = true
+            homeView.menuCollectionView.isHidden = true
+        }
+    }
 }
 
 extension HomeViewController: UICollectionViewDataSource {
